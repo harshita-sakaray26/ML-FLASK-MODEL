7c49f6edf2eebcbcc1a47c842fe44542c4a49c80
munirajgowda <munirajgowdaraj@gmail.com>
2025-10-21 13:09:09 +0530

    initial commit

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def api_disease():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    flask_app.run(host="0.0.0.0", port=port)
